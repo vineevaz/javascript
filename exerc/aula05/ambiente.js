d3407635607d3c2d0f2b17581fdc456692b07211
vineevaz <164183750+vineevaz@users.noreply.github.com>
2024-05-27 09:47:48 -0300

    While & For

---
+++ exerc/aula05/ambiente.js
@@ -0,0 +1,14 @@
+/* console.log('Td Bem?) */
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
+
+/* 
+var c = 1
+while( c <= 2){
+    console.log(`Passo ${c}`)
+    c++
+} 
+*/
---
+++ exerc/aula05/ambiente02.js
@@ -0,0 +1,15 @@
+/*
+var c = 1
+while('c <= 4'){
+    console.log(c)
+    c++
+}
+*/
+//----------------------
+
+console.log('Vai começar...')
+for(var c = 1 ; c <= 5; c++){
+    console.log(c)
+}
+console.log('Fim!')
+
