2ef33fd6ed3a5c9fe3f9ac0245e2bc6e8044190d
vineevaz <164183750+vineevaz@users.noreply.github.com>
2024-06-11 09:09:54 -0300

    Update script.js

--- exerc/aula05/ex017/script.js
+++ exerc/aula05/ex017/script.js
@@ -4,10 +4,11 @@
 
     if(num.value.length == 0){
         window.alert('Por favor, digite um numero')
-        tab.innerHTML = ''
+        
     } else {
         let n = Number(num.value) 
         let c = 1
+        tab.innerHTML = ''
         while (c <= 10) {
             let item = document.createElement('option')
             item.text = `${n} x  ${c} = ${n*c}`
