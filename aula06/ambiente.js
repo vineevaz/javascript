46e2783e4783a389c5936f54b81d499bd9ef72f6
vineevaz <164183750+vineevaz@users.noreply.github.com>
2024-06-11 09:44:36 -0300

    save

---
+++ aula06/ambiente.js
@@ -0,0 +1,12 @@
+let num = [5, 8, 9, 2]
+num.push(1)
+num.sort() //Mostra o vetor em ordem.
+console.log(`Nosso vetor tem ${num.length} posições`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
+
+
+/* 
+Para adicionar um espaço use num[3] = 6 "O valor 6 vai ser criado no terceiro elemento ou vc pode usar o num.push(7) e cria outra chave no ultimo elemento" 
+
+num.length e usado para o comprimento dele 
+*/
---
+++ aula06/vetornatela.js
@@ -0,0 +1,11 @@
+let valores = [3,5,1,3,8]
+valores.sort()
+for(let pos in valores){
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+}
+/*
+for(let pos=0; pos < valores.length; pos++){
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+}
+*/
+// console.log(valores)
