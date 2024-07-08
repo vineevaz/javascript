91e789718658c2ee455d0bb449596fd74347a547
vineevaz <164183750+vineevaz@users.noreply.github.com>
2024-07-08 10:11:47 -0300

    TOP

--- estudo/tocador-de-musica/script.js
+++ estudo/tocador-de-musica/script.js
@@ -11,27 +11,31 @@ const shuffleButton = document.getElementById('shuffle');
 const repeatButton = document.getElementById('repeat');
 const songTime = document.getElementById('song-time')
 const totalTime = document.getElementById('total-time')
+const likeButton = document.getElementById('like')
 
 const churrasco = {
     songName : 'Churrasco de Tuigay',
     artist : 'Bamor',
-    File : 'Churrasco-de-Tuigay'
+    File : 'Churrasco-de-Tuigay',
+    Liked: false
 }
 const bamorTlgd = {
     songName : 'Aqui so dar Bamor',
     artist : 'Bamor',
-    File : 'bamor-tlgd'
+    File : 'bamor-tlgd',
+    Liked: false
 }
 const seFicarInferno = {
     songName : 'Se ficar vai ter duelo',
     artist : 'Inferno Coral',
-    File : 'se-ficar-inferno'
+    File : 'se-ficar-inferno',
+    Liked: false
 }
 
 let isPlaying = false;
 let isShuffled = false;
 let repeatOn = false;
-const originalPaylist = [churrasco, bamorTlgd, seFicarInferno]
+const originalPaylist = JSON.parse(localStorage.getItem('playlist')) ?? [Churrasco-de-Tuigay, bamor-tlgd, se-ficar-inferno ];
 let sortedPlaylist = [...originalPaylist];
 let index =  0
 
@@ -64,6 +68,7 @@ function initializeSong() {
     song.src = `song/${sortedPlaylist[index].File}.mp3`;
     songName.innerText = sortedPlaylist[index].songName;
     bandName.innerText = sortedPlaylist[index].artist;
+    likeButtonRender();
 }
 
 function previousSong(){
@@ -151,6 +156,27 @@ function updateTotalTime(){
     totalTime.innerHTML = toHHMMSS(song.duration);
 
 }
+function likeButtonRender(){
+  if(sortedPlaylist[index].Liked === true){
+    likeButton.querySelector('.bi').classList.remove('bi-heart');
+    likeButton.querySelector('.bi').classList.add('bi-heart-fill');
+    likeButton.classList.add('button-active');
+  } else {
+    likeButton.querySelector('.bi').classList.add('bi-heart');
+    likeButton.querySelector('.bi').classList.remove('bi-heart-fill');
+    likeButton.classList.remove('button-active');
+  }
+}
+function likeButtonClicked(){
+    if(sortedPlaylist[index].Liked === false){
+        sortedPlaylist[index].Liked = true;
+    }else{
+        sortedPlaylist[index].Liked = false;
+    }
+    likeButtonRender();
+    localStorage.setItem('playlist', JSON.stringify(originalPaylist));
+}
+
 
 initializeSong();
 
@@ -163,3 +189,4 @@ song.addEventListener('loadedmetadata', updateTotalTime)
 progressContainer.addEventListener('click', jumpTo)
 shuffleButton.addEventListener('click', shuffleButtonClicked)
 repeatButton.addEventListener('click', repeatButtonClicked)
+likeButton.addEventListener('click', likeButtonClicked)
