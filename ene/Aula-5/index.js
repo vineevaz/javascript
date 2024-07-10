98a1fae95bfd29f2102332eab26b770fb910ecaf
vineevaz <164183750+vineevaz@users.noreply.github.com>
2024-07-10 09:55:40 -0300

    ok

---
+++ ene/Aula-5/index.js
@@ -0,0 +1,8 @@
+// USE SEMPRE O PONTO E VIRGULA !!!
+
+var a = 1;
+var b = 4;
+var c = a + b;
+
+
+document.getElementById('texto').innerHTML = c
---
+++ ene/Aula-5/var-let-const.js
@@ -0,0 +1,16 @@
+// VARIÁVEIS 
+
+/*
+-> var
+-> let não permite delcarar novamente uma variavel
+-> const ela não muda nunca 
+*/
+
+//DECLARAÇÂO DE VARIAVEIS
+var a,b,c;
+//ATRIBUIÇÂO DE VALORES
+// a = 2;
+// b = 3;
+// c = a - b;
+
+console.log(c);
