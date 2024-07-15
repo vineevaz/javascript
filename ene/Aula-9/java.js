58c5523cde4ed4c2cbe2b559b58c772aa8576289
vineevaz <164183750+vineevaz@users.noreply.github.com>
2024-07-15 09:53:09 -0300

    save

---
+++ ene/Aula-9/java.js
@@ -0,0 +1,14 @@
+//USE O PONTO E VIRGULA
+function on(){
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "White";
+
+}
+function dblClick(){
+    alert('Aqui foi Click Duplo');
+}
+
+function mouseMoveTest(){
+    let mouseTest = document.getElementById("test")
+    mouseTest.style.backgroundColor = "green"
+}
